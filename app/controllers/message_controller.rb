38409cb8bfa5e1d242eb3dda43d6f0406d4f5566
Ben Ede <ben.ede@gmail.com>
2014-03-07 20:24:50 +0000

    now started to message

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -15,7 +15,7 @@ class MessageController < ApplicationController
 
   	output = get_local_messages(expiry)
 
-  	local_devices = get_local_devices(expiry, device)
+
 
   	respond_to do |format|
     	format.json  { render :json => output }
@@ -33,6 +33,18 @@ class MessageController < ApplicationController
   	msg.longitude = params[:longitude]
   	msg.device_id = Device.where(token: params[:token]).first
   	msg.ip = request.remote_ip
+
+  	# get out device
+  	device = Device.where(token: params[:token]).first
+
+
+  	# set message cut off
+  	expiry = Time.now - 2.hours
+  	local_devices = get_local_devices(expiry, device)
+
+  	pm = Messenger::PushMessenger.new
+  	pm.send_message(msg.message, local_devices)
+
 
   	if msg.save
 	    output = { :status => "ok"}
@@ -65,8 +77,18 @@ protected
   end
 
   def get_local_devices(expiry, device)
-		devices = ActiveRecord::Base.connection.execute("select device_id from ( select ( 6371 * acos( cos( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}.#{params[:lon_dec]}) ) + sin( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from locations a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' and device_id <> #{device.id} order by created_at desc")
-		logger.info("DEVICES IN AREA #{devices.to_a}")
+		devices_ids = ActiveRecord::Base.connection.execute("select device_id from ( select ( 6371 * acos( cos( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}.#{params[:lon_dec]}) ) + sin( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from locations a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' and device_id <> #{device.id} order by created_at desc")
+		logger.info("DEVICES IN AREA #{devices_ids.to_a}")
+
+		devices = Array.new
+
+		devices_ids.each do |d|
+			dev = Device.find_by_id(d)
+			logger.info("DEvice token: #{dev.token}")
+			devices.push(dev)
+		end
+
+		devices
 	end
 
 	def record_location(device)
---
+++ lib/messenger/push_messenger.rb
@@ -0,0 +1,86 @@
+
+
+class Messenger::PushMessenger
+
+	def send_message(msg, devices)
+		android = Array.new
+		ios = Array.new
+
+		devices.each do |device|
+			if device.platform == 'and'
+				android.push(device)
+			elsif device.platform == 'ios'
+				ios.push(device)
+			end
+		end
+
+		if ios.size > 0
+			send_android(msg, android)
+		end
+		if ios.size > 0
+			send_ios(msg, ios)
+		end
+	end
+
+protected
+	def send_android(msg, devices)
+  	# this is the apiKey obtained from here https://code.google.com/apis/console/
+  	#destination = "APA91bFdVQPOCiDVaw50g3fCWYhkArfXgEeY4CDNumk7EGNSIgRDFEOScuGddYi4XIHpo7QGpgGxVtcZ0crl1NVvEpM5omx9A42v6vS9FYjetBGpMYYJw64-AP7VQQsNfKmefvbKXMQTSaeq4bW5iqrZUu-Qi6lltA"
+    destination = Array.new
+    
+    devices.each do |d|
+      destination.push(d.token)
+    end
+
+
+    # can be an string or an array of strings containing the regIds of the devices you want to send
+
+    data = {:message => msg, :key2 => ["array", "value"]}
+    # must be an hash with all values you want inside you notification
+
+    #GCM.send_notification( destination )
+    # Empty notification
+
+    res = GCM.send_notification( destination, data )
+    Rails.logger.info("RES #{res}")
+    # Notification with custom information
+
+    #GCM.send_notification( destination, data, :collapse_key => "placar_score_global", :time_to_live => 3600, :delay_while_idle => false )
+    # Notification with custom information and parameters
+
+  end
+
+	def send_ios(msg, devices)
+
+
+		
+		apn = Houston::Client.development
+		apn.certificate = File.read(Rails.configuration.ios_pem)
+
+    destination = Array.new
+    
+    devices.each do |d|
+      # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
+      notification = Houston::Notification.new(device: d.token)
+      notification.alert = msg
+
+      # Notifications can also change the badge count, have a custom sound, indicate available Newsstand content, or pass along arbitrary data.
+      notification.badge = 0
+      notification.sound = "sosumi.aiff"
+      notification.content_available = true
+      
+      destination.push(notification)
+    end
+
+
+
+		
+
+		# And... sent! That's all it takes.
+		apn.push(destination)
+		
+
+	end
+
+end
+
