5af48d587a0e95f7f9e79f7cbcc33cbbe922fb0f
Ben Ede <ben.ede@gmail.com>
2014-03-07 19:40:03 +0000

    formateted output

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -6,18 +6,24 @@ class MessageController < ApplicationController
   	logger.info("Get called #{params[:latitude]}, #{params[:longitude]}")
 
   	# set message cut off
-  	expiry = Time.now - 7200
-
-  	expiry = Time.now - 1.hour
+  	expiry = Time.now - 2.hours
 
   	messages = ActiveRecord::Base.connection.execute("select distance, message, created_at from ( select ( 6371 * acos( cos( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * cos( radians( a.latitude ) ) * cos( radians( a.longitude ) - radians(#{params[:longitude]}.#{params[:lon_dec]}) ) + sin( radians(#{params[:latitude]}.#{params[:lat_dec]}) ) * sin( radians( a.latitude ) ) ) ) as distance, a.* from messages a ) as dt where distance < 0.2 and created_at > '#{expiry.strftime('%Y-%m-%d %H:%M:%S')}' order by created_at desc")
 
   	logger.info("MESSAGES: #{messages.size}")
 
-  	
+  	output = Array.new
+  	messages.each do |m|
+  		logger.info("M: #{m}")
+  		h = Hash.new
+  		h['m'] = m[1]
+  		h['d'] = m[0]
+  		h['t'] = 7200 - ((Time.now ) - m[2])
+  		output.push(h)
+  	end
 
   	respond_to do |format|
-    	format.json  { render :json => messages }
+    	format.json  { render :json => output }
   	end
 
   end
